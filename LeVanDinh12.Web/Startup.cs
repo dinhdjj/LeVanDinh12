db3a1510e59515283bd96e4dc16d93079343e5b5
Le Dinh <64189902+dinhdjj@users.noreply.github.com>
2022-08-22 16:18:47 +0700

    setup swagger

--- LeVanDinh12.Web/Startup.cs
+++ LeVanDinh12.Web/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace LeVanDinh12.Web
 {
@@ -26,6 +27,30 @@ namespace LeVanDinh12.Web
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+
+			#region -- Swagger --  
+            var inf1 = new OpenApiInfo
+            {
+                Title = "API v1.0",
+                Version = "v1",
+                Description = "Swashbuckle",
+                TermsOfService = new Uri("http://appointvn.com"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Le Dinh",
+                    Email = "dinhdjj@gmail.com"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Apache 2.0",
+                    Url = new Uri("http://www.apache.org/licenses/LICENSE-2.0.html")
+                }
+            };
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", inf1);
+            }); 
+            #endregion
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +60,14 @@ namespace LeVanDinh12.Web
 			{
 				app.UseDeveloperExceptionPage();
 			}
+
+			#region -- Swagger --
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1.0");
+            });
+            #endregion
 
 			app.UseHttpsRedirection();
 
