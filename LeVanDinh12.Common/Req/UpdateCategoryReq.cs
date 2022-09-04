6c80086ce53599096f7670e3e5c1626fe98d806e
tandat0723 <71442386+tandat0723@users.noreply.github.com>
2022-09-04 21:18:16 +0700

    feat(category): CURD (#2)
    * category
    
    * category post
    
    Co-authored-by: dat tran <tandat@tandattran.onmicrosoft.com>

---
+++ LeVanDinh12.Common/Req/UpdateCategoryReq.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeVanDinh12.Common.Req
+{
+    public class UpdateCategoryReq
+    {
+        public string oldName { get; set; }
+        public string newName { get; set; }
+        public string Description { get; set; }
+    }
+}
